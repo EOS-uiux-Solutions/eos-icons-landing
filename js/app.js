7cd8af9f14900236fced065bc06a83df720bfb8c
Sorin <scurescu@suse.com>
2019-06-03 10:29:57 +0200

    Fixed error

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 (function () {
   let showCodeSnippet = false // eslint-disable-line no-unused-vars
-  let iconName, iconDemo, iconSize, iconClass
+  let iconName, iconSize, iconDemo, iconClass
 
   // hide code snippet by default
   document.getElementsByClassName('how-to-use')[0].style.visibility = 'hidden'
@@ -13,7 +13,6 @@
 
     if (iconName) {
       showCodeSnippet = true
-      console.log('something selected')
       document.getElementsByClassName('how-to-use')[0].style.visibility = 'visible'
       document.getElementsByClassName('how-to-use-animated')[0].style.visibility = 'hidden'
     }
@@ -22,12 +21,13 @@
   document.getElementById('icons-animated').onclick = function (e) {
     e = e || window.event
     iconName = e.target.getAttribute('data-name') || e.target.parentNode.getAttribute('data-name')
-    document.getElementById('animatedIconDemo').innerHTML = iconDemo
+
     if (iconName) {
       showCodeSnippet = true
-      iconDemo = `<pre><code><i class=&quoteos-icons eos-icon-${iconName}> </i></code></pre>`
+      iconDemo = `<pre><code>i class=&quoteos-icons eos-icon-${iconName} ${iconName} ></code></pre>`
       document.getElementsByClassName('how-to-use-animated')[0].style.visibility = 'visible'
     }
+    document.getElementById('animatedIconDemo').innerHTML = iconDemo
   }
 
   document.getElementById('close-code-snippet').onclick = function (e) {
@@ -50,23 +50,47 @@
       e = e || window.event
       iconSize = e.target.getAttribute('data-size')
       iconClass = `md-${iconSize}`
-      for (let y = 0; y < document.getElementsByClassName('eos-icons').length; y++) {
-        document.getElementsByClassName('eos-icons')[y].style.fontSize = `${iconSize}px`
+      for (i = 0; i < document.getElementsByClassName('eos-icons').length; i++) {
+        document.getElementsByClassName('eos-icons')[i].style.fontSize = `${iconSize}px`
         document.getElementById('eos-icon-installing').className = `eos-icons eos-icon-installing${iconClass}`
       }
     }
   }
+
+  const $iconsContainer = $('.js-icons-list')
+  const $iconDisplayTemplate = $('.js-icons-item').clone(true)
+  $('.js-icons-item').remove()
+
+  const iconsListData = () => {
+    getIconsList(function (data) { // eslint-disable-line
+      const iconsList = data.glyphs
+
+      for (let i = 0; i < iconsList.length; i++) {
+        const newIconDisplay = $iconDisplayTemplate.clone(true)
+        const iconName = iconsList[i]
+
+        // Add icon name
+        $(newIconDisplay).attr('data-name', iconName)
+        $(newIconDisplay).find('.js-eos-icons').text(iconName).addClass(`eos-icon-${iconName}`)
+        $(newIconDisplay).find('.js-eos-icon-name').text(iconName)
+
+        $($iconsContainer).append(newIconDisplay)
+      }
+    })
+  }
+
+  iconsListData()
 })()
 
 const searchIcon = () => { // eslint-disable-line no-unused-vars
-  const input = document.getElementsByClassName('searchbar')[0].value
+  input = document.getElementsByClassName('searchbar')[0].value // eslint-disable-line
   const x = document.getElementsByClassName('icons-item')
-
-  for (let i = 0; i < x.length; i++) {
-    if (!(x[i].getElementsByClassName('eos-icons')[0].innerHTML.includes(input))) {
-      x[i].style.display = 'none'
-    } else {
-      x[i].style.display = 'inline-block'
+  for (i = 0; i < x.length; i++) { // eslint-disable-line no-undef
+    if (!(x[i].getElementsByClassName('eos-icons')[0].innerHTML.includes(input))) { // eslint-disable-line no-undef
+      x[i].style.display = 'none' // eslint-disable-line no-undef
+    } // eslint-disable-line brace-style
+    else {
+      x[i].style.display = 'inline-block' // eslint-disable-line no-undef
     }
   }
 }
