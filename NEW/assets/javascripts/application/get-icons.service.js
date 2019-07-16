ccad1af3cc812824fc992bda1e423b389372d468
Cynthia Sanchez <csanchez@suse.com>
2019-07-16 16:20:55 +0200

    Added the extended files, but they require fixes once the Eos-icons package is updated

--- NEW/assets/javascripts/application/get-icons.service.js
+++ NEW/assets/javascripts/application/get-icons.service.js
@@ -11,3 +11,23 @@ const getIconsList = (callback) => {
     callback(dataGlyph)
   })
 }
+
+/* This is not working for now since we need to fix the problem
+ with the extended library name first.
+ Leaving this commented for now so we don't forget to add this service
+ ↓ ↓ ↓
+ */
+
+// const getExtendedIconsList = (callback) => {
+//   $.ajax({
+//     url: 'vendors/js/glyph-list-ext.json',
+//     method: 'get',
+//     dataType: `json`,
+//     error: function (xhr, status, error) {
+//       console.log(`there was an error retrieving the icons collection`)
+//     }
+//   })
+//   .then(function (dataGlyph) {
+//     callback(dataGlyph)
+//   })
+// }
