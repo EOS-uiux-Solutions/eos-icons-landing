3aca34e454fb133b47703ea9a0abb6ec13dbbc5f
Sorin <scurescu@suse.com>
2020-01-29 16:45:26 +0100

    React routes

--- eos-icons-react/src/App.js
+++ eos-icons-react/src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
+import { Router, Link } from "@reach/router"
 import './App.css';
+import Home from './components/Home'
+import Cheatsheet from './components/Home'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+    <Router>
+      <Home path="/" />
+      <Cheatsheet path="cheatsheet" />
+    </Router>
+  )
 }
 
 export default App;
---
+++ eos-icons-react/src/components/Customize.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Customize () {
+  return (
+    <h1>Customize</h1>
+  );
+}
+
+export default Customize;
