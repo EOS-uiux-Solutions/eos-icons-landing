11605a78319f039a168670886034a9fc32dc73db
Sorin <scurescu@suse.com>
2020-01-29 16:58:41 +0100

    Routes, header, footer

--- eos-icons-react/src/App.js
+++ eos-icons-react/src/App.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import { Router, Link } from "@reach/router"
 import './App.css';
-import Home from './components/Home'
-import Cheatsheet from './components/Home'
+
+/* Pages */
+import Home from './pages/Home'
+import Cheatsheet from './pages/Cheatsheet'
+import Customize from './pages/Customize'
+import Docs from './pages/Docs'
+
+/* Componets */
+import Footer from './components/Footer'
 
 function App() {
   return(
-    <Router>
-      <Home path="/" />
-      <Cheatsheet path="cheatsheet" />
-    </Router>
+    <div className="App">
+      <div>
+        <nav>
+          <Link to="/">Home</Link> |{" "}
+          <Link to="/cheatsheet">Cheatsheet |{" "}</Link>
+          <Link to="/customize">Customize |{" "}</Link>
+          <Link to="/docs">Docs </Link>
+        </nav>
+      </div>
+      <Router>
+        <Home path="/" />
+        <Cheatsheet path="/cheatsheet" />
+        <Customize path="/customize" />
+        <Docs path="/docs" />
+      </Router>
+
+      <Footer />
+    </div>
   )
 }
 
