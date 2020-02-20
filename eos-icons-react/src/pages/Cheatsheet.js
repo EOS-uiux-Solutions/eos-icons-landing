1e473cf3c18420ff48dddb6d46f4655aa92923ff
Sorin <scurescu@suse.com>
2020-02-20 16:09:06 +0100

    New: Header component

--- eos-icons-react/src/pages/Cheatsheet.js
+++ eos-icons-react/src/pages/Cheatsheet.js
@@ -3,8 +3,10 @@ import PageHeader from '../components/PageHeader'
 
 const Cheatsheet = () => {
   return (
-    <PageHeader>
-      Cheatsheet: find the ligature of the icon you are looking for, plus, get an overview of all the available icons.
+    <PageHeader theme="purple">
+      <h2>
+        Cheatsheet: find the ligature of the icon you are looking for, plus, get an overview of all the available icons.
+      </h2>
     </PageHeader>
   );
 }
--- eos-icons-react/src/pages/Customize.js
+++ eos-icons-react/src/pages/Customize.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import PageHeader from '../components/PageHeader'
 
 function Customize () {
   return (
-    <h1>Customize</h1>
+    <PageHeader theme="purple">
+      <h2>Click on Icons to select them</h2>
+      <p>
+        To continue building an old font, upload icons-config.json
+        <button> file here </button>
+      </p>
+    </PageHeader>
   );
 }
 
