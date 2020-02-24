3b74494c71f4409c9cf8a0b2b8a2c537f1df4b86
Kartikay Bhutani <kbhutani0001@gmail.com>
2020-02-25 01:32:23 +0530

    added search bar UI only

--- eos-icons-react/src/pages/Cheatsheet.js
+++ eos-icons-react/src/pages/Cheatsheet.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import PageHeader from '../components/PageHeader'
+import SearchIcon from '../components/SearchIcon'
 
 const Cheatsheet = () => {
   return (
+    <div>
     <PageHeader theme="purple">
       <h2>
         Cheatsheet: find the ligature of the icon you are looking for, plus, get an overview of all the available icons.
       </h2>
     </PageHeader>
+    <SearchIcon />
+    </div>
+  
   );
 }
 
