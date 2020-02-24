54e6d2ad657b44d1ee9587000d293ed7b1a6fcd3
Cynthia Sanchez <csanchez@suse.de>
2020-02-24 19:50:31 +0100

    add refactor suggested for argument destructuring

--- eos-icons-react/src/components/Button.js
+++ eos-icons-react/src/components/Button.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
-const Button = props => {
-  const { children, primary, action } = props
+const Button = ({ children, primary, action }) => {
   const btnClass = primary ? 'btn-primary' : 'btn-default'
   return (
     <button className={`btn ${btnClass}`} action={action} >
--- eos-icons-react/src/components/Link.js
+++ eos-icons-react/src/components/Link.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
-const Link = props => {
-  const { href, name, external, button } = props
+const Link = ({ href, name, external, button }) => {
   const target = external ? '_blank' : '_self'
   const btnClass = button ? button === 'primary' ? 'btn btn-primary' : 'btn btn-default' : ''
   return (<a href={href} target={target} className={btnClass}>{name}</a>)
