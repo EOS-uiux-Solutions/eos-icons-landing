599c761b8663f24feb73aea916587bbc9b70c73a
Sorin <scurescu@suse.com>
2020-02-25 10:47:47 +0100

    Single icon selection active class

--- eos-icons-react/src/pages/Cheatsheet.js
+++ eos-icons-react/src/pages/Cheatsheet.js
@@ -1,11 +1,17 @@
-import React, { useContext, useReducer } from 'react';
+import React, { useContext, useReducer, useState, useEffect } from 'react';
 import PageHeader from '../components/PageHeader'
 import { EosIconStore, iconsReducer } from '../utils/EosIcons.store'
 
+const setCass = props => {
+  const { name, selected } = props
+  console.log('selected: ', selected);
+  console.log('name: ', name);
+
+  return name !== selected ? 'active' : ''
+}
 const Cheatsheet = props => {
   const value = useContext(EosIconStore)
   const [state, dispatch] = useReducer(iconsReducer, value)
-  console.log('state: ', state);
 
   return (
     <div>
@@ -16,23 +22,20 @@ const Cheatsheet = props => {
       </PageHeader>
 
       {/* ============ DEMO ============ */}
-      <div style={{ margin: 20 }}>{value.singleIcon.map((ele, i) => {
-        return <pre key={i}>Icon: {ele}</pre>
-      })}
+      <div style={{ margin: 20 }}>
+        {value.singleIcon.map((ele, i) => <pre key={i}>Icon: {ele}</pre>)}
 
-        <pre>Details: {state.icons.map(ele => {
-          return ele.name === state.singleIcon[0] ? JSON.stringify(ele, null, 2) : ''
-        })}</pre>
+        <pre>Details: {state.icons.map(ele => ele.name === state.singleIcon[0] ? JSON.stringify(ele, null, 2) : '')}</pre>
 
         <div style={{ display: 'flex', margin: 10, flexWrap: 'wrap' }}>
           {
             state.icons.map((ele, index) => {
               return (ele.name === 'installing' || ele.name === 'loading')
                 ? ''
-                : <i className="eos-icons" key={index} onClick={e => {
+                : <i className={`eos-icons ${state.singleIcon[0] === ele.name ? 'active' : ''}`} key={index} onClick={e => {
                   e.preventDefault()
                   return dispatch({ type: 'ADD_ICON', selection: e.target.outerText })
-                }}>{ele.name}</i>
+                }}> {ele.name} </i>
             })
           }
         </div>
