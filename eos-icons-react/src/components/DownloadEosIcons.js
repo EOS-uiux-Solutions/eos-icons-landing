cee3530bffc5fa24e7c29c2f68a105e7e5fccba2
Aditya Sharma <sharmaaditya570191@gmail.com>
2020-02-26 12:53:21 +0530

    Added alt message

--- eos-icons-react/src/components/DownloadEosIcons.js
+++ eos-icons-react/src/components/DownloadEosIcons.js
@@ -7,7 +7,11 @@ const DownloadEosIcons = () => {
       className='download-eos-icons-button'
       href='https://www.npmjs.com/package/eos-icons'
     >
-      <img className='download-icons-logo' src={downloadIcon} alt='' />
+      <img
+        className='download-icons-logo'
+        src={downloadIcon}
+        alt='download icons'
+      />
       <strong>
         <small className='download-icons-text'>download-EOS-icons-set</small>
       </strong>
