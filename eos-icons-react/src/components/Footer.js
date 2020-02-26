41b9e18700bbe764d2eb557f6a4f6af0b7f6d131
r-kohale9 <r.kohale9@gmail.com>
2020-02-27 01:48:49 +0530

    data event attributes added to Link component

--- eos-icons-react/src/components/Footer.js
+++ eos-icons-react/src/components/Footer.js
@@ -4,23 +4,38 @@ import Link from './Link'
 const footerLinks = [
   {
     name: 'Join us on Slack',
-    href: 'https://slack.eosdesignsystem.com/'
+    href: 'https://slack.eosdesignsystem.com/',
+    category: 'External link',
+    action: 'Link to EOS Slack',
+    label: 'Footer'
   },
   {
     name: 'Repository',
-    href: 'https://gitlab.com/SUSE-UIUX/eos-icons'
+    href: 'https://gitlab.com/SUSE-UIUX/eos-icons',
+    category: 'External link',
+    action: 'Link to Gitlab repo',
+    label: 'Footer'
   },
   {
     name: 'Report a issue',
-    href: 'https://gitlab.com/SUSE-UIUX/eos-icons/issues'
+    href: 'https://gitlab.com/SUSE-UIUX/eos-icons/issues',
+    category: 'External link',
+    action: 'Link to Gitlab issues',
+    label: 'Footer'
   },
   {
     name: 'EOS on Twitter',
-    href: 'https://twitter.com/eosdesignsystem'
+    href: 'https://twitter.com/eosdesignsystem',
+    category: 'External link',
+    action: 'EOS in Twitter',
+    label: 'Footer'
   },
   {
     name: 'Cookie policy',
-    href: 'https://eos-icons.eosdesignsystem.com/cookies-policy.html'
+    href: 'https://eos-icons.eosdesignsystem.com/cookies-policy.html',
+    category: 'External link',
+    action: '',
+    label: 'Footer'
   }
 ]
 
--- eos-icons-react/src/components/Link.js
+++ eos-icons-react/src/components/Link.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const Link = props => {
-  const { href, name } = props
+  const { href, name, category, action, label } = props
 
-  return (<a href={href}>{name}</a>)
+  return (<a href={href} data-event-category={category} data-event-label={label} data-event-action={action} >{name}</a>)
 }
 
 export default Link
