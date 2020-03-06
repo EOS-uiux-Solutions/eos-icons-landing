fd5e985bae3d01a3e36550cf15a233fa2a1854e7
Sorin <scurescu@suse.com>
2020-03-06 11:26:48 +0100

    cleaning

--- eos-icons-react/src/components/HowToPanel.js
+++ eos-icons-react/src/components/HowToPanel.js
@@ -1,41 +1,39 @@
-import React, { useContext, useReducer } from 'react';
-
+import React from 'react';
 
 const HowToPanel = props => {
-    const state = props.state;
-    
-    return (
-            <>
-            {
-                <div className="how-to-use-container">
-                    <h2 className="h2-how-to-use">How to use it:
-                        <small className="right-pulled-small" id="close-code-snippet">
-                        <i className="eos-icons md-18">close</i></small>
-                    </h2>
-                    <div className="how-to-use-input-group">
-                        <input className="how-to-use-curved-control" id="copy3" readOnly="readOnly" value = {`<i className='eos-icons'> ${state.singleIcon[0]} </i>`} />
-                        <div className="how-to-use-input-group-append">
-                            <button className="how-to-use-copy-button" id="copy1" type="button" onClick={ () => { document.getElementById("copy3").select(); document.execCommand('copy'); }}>
-                                <i className="eos-icons md-18">content_copy</i> Copy</button>
-                        </div>
-                    </div>
-                    <div className="how-to-use-row">
-                        <b className="ml-3">Tags:</b>
-                        <div className="tags">
-                            {state.icons.map(
-                                (icon) => {
-                                    if(icon.name === state.singleIcon[0])
-                                    return icon.tags.map((tag) => {
-                                        return <span className="how-to-use-badge"> {tag} </span>
-                                    })
-                                }
-                            )}
-                        </div>
-                    </div>
-                </div>
-            }          
-            </>
-    )
+  const { state } = props;
+
+  return (
+    <>
+      {
+        <div className="how-to-use-container">
+          <h2 className="h2-how-to-use">How to use it:
+              <small className="right-pulled-small" id="close-code-snippet">
+              <i className="eos-icons md-18">close</i></small>
+          </h2>
+          <div className="how-to-use-input-group">
+            <input className="how-to-use-curved-control" id="copy3" readOnly="readOnly" value={`<i className='eos-icons'> ${state.singleIcon[0]} </i>`} />
+            <div className="how-to-use-input-group-append">
+              <button className="how-to-use-copy-button" id="copy1" type="button" onClick={() => { document.getElementById("copy3").select(); document.execCommand('copy'); }}>
+                <i className="eos-icons md-18">content_copy</i> Copy</button>
+            </div>
+          </div>
+          <div className="how-to-use-row">
+            <b className="ml-3">Tags:</b>
+            <div className="tags">
+              {state.icons.map(icon => {
+                return icon.name === state.singleIcon[0]
+                  ? icon.tags.map((tag, key) => {
+                    return <span key={key} className="how-to-use-badge"> {tag} </span>
+                  })
+                  : ''
+              })}
+            </div>
+          </div>
+        </div>
+      }
+    </>
+  )
 }
 
 export default HowToPanel
