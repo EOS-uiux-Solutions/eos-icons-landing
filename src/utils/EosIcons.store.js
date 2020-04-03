1992bba54524702da8a988f03a965b05f69c673a
bisht-richa <richa.bisht@suse.com>
2020-04-03 12:52:23 +0200

    Merged master

--- src/utils/EosIcons.store.js
+++ src/utils/EosIcons.store.js
@@ -2,9 +2,6 @@ import { createContext } from 'react'
 import eosIcons from 'eos-icons/dist/js/eos-icons.json'
 import extendedIcons from 'eos-icons/dist/extended/js/glyph-list.json'
 import animatedIcons from './AnimatedIcons.store.js'
-
-import Cookies from 'js-cookie'
-
 import Cookies from 'js-cookie'
 
 const multipleIcons = []
