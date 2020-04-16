1115a3aa8d9bb5c8b968c670ac0936fa53ef258f
Sorin <scurescu@suse.com>
2020-04-16 13:00:20 +0200

    lint and spelling

--- src/components/CookiesBanner.js
+++ src/components/CookiesBanner.js
@@ -29,8 +29,8 @@ const CookiesBanner = () => {
           >
             <div className='cookies-alert-body'>
               <p>
-                EOS Icons uses cookies to help us learn more about how we
-                can improve our product.
+                EOS Icons uses cookies to help us learn more about how we can
+                improve our product.
                 <br />
                 <a href='/cookies-policy'>Learn more about our cookie policy</a>
               </p>
