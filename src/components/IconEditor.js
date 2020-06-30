19b8acd576cecb2193c72fd50430144e850064cb
Shashank Jarial <shashankjarial10@gmail.com>
2020-06-30 17:39:03 +0530

    download icon set on export

--- src/components/IconEditor.js
+++ src/components/IconEditor.js
@@ -4,37 +4,44 @@ import Button from './Button'
 import axios from 'axios'
 
 const IconEditor = (props) => {
+  const apiBaseUrl = 'https://eos-icons-picker-api.herokuapp.com/'
+
   const { isActive, show, iconName } = props
 
   const [color, setColor] = useState('#000000')
 
   const changeColor = (color) => {
     setColor(color.hex)
-    document.getElementsByClassName('icon-preview')[0].style.color = color.hex;
+    document.getElementsByClassName('icon-preview')[0].style.color = color.hex
   }
-  const [serverResponse, setServerResponse] = useState(null)
+  // const [serverResponse, setServerResponse] = useState(null)
 
   const postDataToApi = async (params) => {
     const { url, payload } = params
-  
+
     const response = await axios.post(url, payload)
     return response.data
   }
   const downloadCustomizedIcon = (props) => {
     const { timestamp } = props
-    const downloadEndPoints = `https://eos-icons-picker-api.herokuapp.com/download?ts=${timestamp}`
+    const downloadEndPoints = `${apiBaseUrl}download?ts=${timestamp}`
     return window.open(downloadEndPoints, '_blank')
   }
   const generateCustomizedIcon = (e) => {
     e.preventDefault()
     postDataToApi({
-      url: 'localhost:3131/icon-customization',
+      url: `${apiBaseUrl}icon-customization`,
       payload: {
-        "icons": [iconName],
-        "exportAs": "svg",
-        "customizationConfig": {"colorCode": color}
+        icons: [iconName],
+        exportAs: 'svg',
+        customizationConfig: { colorCode: color }
       }
-    }).then((setServerResponse))
+    }).then((res) => {
+      // setServerResponse(res)
+      // not using server response for now since the first time state is null on automatic download
+      // will use in upcoming PR
+      downloadCustomizedIcon({ timestamp: res })
+    })
   }
 
   return isActive ? (
@@ -54,14 +61,9 @@ const IconEditor = (props) => {
           <div className='icon-div'>
             <p>Icon Preview</p>
             <div className='icon-preview'>
-              <i className='eos-icons'>
-                {iconName}
-              </i>
+              <i className='eos-icons'>{iconName}</i>
             </div>
-            <Button
-            primary
-            type='button'
-            onClick={generateCustomizedIcon}>
+            <Button primary type='button' onClick={generateCustomizedIcon}>
               <i className='eos-icons eos-18'>file_download</i> Export as SVG
             </Button>
           </div>
