a3d60dafa04486187e62d5dfb0c88b6769c1cb42
Cynthia Sanchez <csanchez@suse.de>
2020-12-01 13:52:18 +0000

    fix close button to have more space to click around it

--- src/components/HowToPanel.js
+++ src/components/HowToPanel.js
@@ -15,29 +15,15 @@ const HowToPanel = (props) => {
 
   function useOnClickOrEsc(ref, handler) {
     useEffect(() => {
-      const listenerMousedown = (event) => {
-        if (
-          !ref.current ||
-          ref.current.contains(event.target) ||
-          event.target.innerHTML === iconName ||
-          event.target.className.includes('eos-icons')
-        )
-          return
-
-        handler(event)
-      }
-
       const listenerKeydown = (event) => {
         if (event.keyCode === 27) {
           handler(event)
         }
       }
 
-      document.addEventListener('mousedown', listenerMousedown)
       document.addEventListener('keydown', listenerKeydown)
 
       return () => {
-        document.removeEventListener('mousedown', listenerMousedown)
         document.removeEventListener('keydown', listenerKeydown)
       }
     }, [ref, handler])
@@ -50,11 +36,9 @@ const HowToPanel = (props) => {
       <div className='container'>
         <span>How to use it:</span>
 
-        <small className='close-button'>
-          <i className='eos-icons eos-18' onClick={() => close()}>
-            close
-          </i>
-        </small>
+        <i className='close-button eos-icons eos-18' onClick={() => close()}>
+          close
+        </i>
 
         {type === 'animated' ? (
           <div className='how-to-use-block-left'>
