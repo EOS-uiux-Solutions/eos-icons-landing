d28e637b7fd6742e42cefbeafa65849a569f40d9
Sorin <scurescu@suse.com>
2020-12-01 12:43:08 +0100

    Added filtered icon when sent as url params

--- src/modules/IconsSet.js
+++ src/modules/IconsSet.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import AppContext from '../utils/AppContext'
 
 /* Components */
@@ -16,6 +16,7 @@ const IconsSet = (props) => {
   const [iconSelected, setIconSelected] = useState('')
   const [showPanel, setShowPanel] = useState(false)
   const [size] = useWindowsSize()
+  const { state, dispatch } = useContext(AppContext)
 
   const [tab, setActiveTab] = useState('Static Icons')
 
@@ -23,16 +24,31 @@ const IconsSet = (props) => {
   const urlParams = new URLSearchParams(queryString)
   const urlIconName = urlParams.get('iconName')
 
+  const setSearchWithUrlParam = urlIconName && !iconSelected ? urlIconName : ''
+
+  const setIconInSearch = () => {
+    return dispatch({
+      type: 'TOGGLE_SEARCH_REGULAR_ICONS',
+      search: urlIconName
+    })
+  }
+
   useEffect(() => {
     if (urlIconName) {
       const iconDetails = eosIconsState.icons.filter(
         (icon) => icon.name === urlIconName
       )
+
+      if (!iconSelected) {
+        setIconInSearch()
+      }
+
       if (iconDetails.length) {
         setShowPanel(true)
         setIconSelected({ name: urlIconName, tags: iconDetails[0].tags })
       }
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [urlIconName])
 
   const closeHowTo = () => {
@@ -68,135 +84,130 @@ const IconsSet = (props) => {
   }
 
   return (
-    <AppContext.Consumer>
-      {({ state, dispatch }) => (
-        <>
-          <PageHeader>
-            <h1>More than 1000 free icons</h1>
-            <div className='icons-control'>
-              <div className='icons-control-search'>
-                <i className='eos-icons'>search</i>
-                <input
-                  className='search-input'
-                  type='text'
-                  name='search'
-                  onChange={(event) =>
-                    dispatch({
-                      type:
-                        tab === 'Static Icons'
-                          ? 'TOGGLE_SEARCH_REGULAR_ICONS'
-                          : 'TOGGLE_SEARCH_ANIMATED_ICONS',
-                      search: event.target.value
-                    })
-                  }
-                />
-                {!size?.isMobile ? (
-                  <CategorySelector disabled={tab === 'Animated Icons'} />
-                ) : (
-                  ' '
-                )}
-              </div>
+    <>
+      <PageHeader>
+        <h1>More than 1000 free icons</h1>
+        <div className='icons-control'>
+          <div className='icons-control-search'>
+            <i className='eos-icons'>search</i>
+            <input
+              defaultValue={setSearchWithUrlParam}
+              className='search-input'
+              type='text'
+              name='search'
+              onChange={(event) => {
+                dispatch({
+                  type:
+                    tab === 'Static Icons'
+                      ? 'TOGGLE_SEARCH_REGULAR_ICONS'
+                      : 'TOGGLE_SEARCH_ANIMATED_ICONS',
+                  search: event.target.value
+                })
+              }}
+            />
+            {!size?.isMobile ? (
+              <CategorySelector disabled={tab === 'Animated Icons'} />
+            ) : (
+              ' '
+            )}
+          </div>
 
-              <div className='icons-control-dynamic'>
-                {size?.isMobile ? (
-                  <CategorySelector disabled={tab === 'Animated Icons'} />
-                ) : (
-                  ' '
-                )}
+          <div className='icons-control-dynamic'>
+            {size?.isMobile ? (
+              <CategorySelector disabled={tab === 'Animated Icons'} />
+            ) : (
+              ' '
+            )}
 
-                <div className='icons-control-toggle'>
-                  <Toogle
-                    disabledStatus={tab === 'Animated Icons'}
-                    name='Select multiple'
-                    id='js-icon-picker'
-                    onClick={() =>
-                      toggleCustomize(dispatch({ type: 'TOGGLE_CUSTOMIZE' }))
-                    }
-                  />
-                </div>
-              </div>
+            <div className='icons-control-toggle'>
+              <Toogle
+                disabledStatus={tab === 'Animated Icons'}
+                name='Select multiple'
+                id='js-icon-picker'
+                onClick={() =>
+                  toggleCustomize(dispatch({ type: 'TOGGLE_CUSTOMIZE' }))
+                }
+              />
             </div>
-            <div className='icon-information'>
-              {!state.customize ? (
-                <div>
-                  <ShowHowToUse
-                    tab={tab}
-                    showPanel={showPanel}
-                    iconSelected={iconSelected}
-                    closeHowTo={closeHowTo}
-                  />
+          </div>
+        </div>
+        <div className='icon-information'>
+          {!state.customize ? (
+            <div>
+              <ShowHowToUse
+                tab={tab}
+                showPanel={showPanel}
+                iconSelected={iconSelected}
+                closeHowTo={closeHowTo}
+              />
+            </div>
+          ) : (
+            <div className='how-to-use-block'>
+              <CustomizeIconsPanel
+                selectAll={() => dispatch({ type: 'ADD_ALL_ICONS' })}
+                deselectAll={() => dispatch({ type: 'REMOVE_ALL_ICONS' })}
+              />
+            </div>
+          )}
+        </div>
+      </PageHeader>
+      <div className='container no-padding'>
+        <Tabs
+          setTab={setActiveTab}
+          customize={state.customize}
+          showPanel={showPanel}
+        >
+          <div label='Static Icons'>
+            {state.iconsCategory.map((categoryObject, index) => {
+              return categoryObject.icons.length > 0 ? (
+                <div key={index}>
+                  <h4 className='category'>{categoryObject.category}</h4>
+                  <div className='icons-list'>
+                    {categoryObject.icons.map((icon, i) => (
+                      <Icon
+                        size={36}
+                        active={isActive(icon.name, state)}
+                        key={i}
+                        name={icon.name}
+                        action={() =>
+                          selectIcon(
+                            icon,
+                            dispatch({
+                              type: state.customize ? 'ADD_MULTIPLE_ICONS' : '',
+                              selection: icon.name
+                            })
+                          )
+                        }
+                      />
+                    ))}
+                  </div>
                 </div>
               ) : (
-                <div className='how-to-use-block'>
-                  <CustomizeIconsPanel
-                    selectAll={() => dispatch({ type: 'ADD_ALL_ICONS' })}
-                    deselectAll={() => dispatch({ type: 'REMOVE_ALL_ICONS' })}
-                  />
-                </div>
-              )}
-            </div>
-          </PageHeader>
-          <div className='container no-padding'>
-            <Tabs
-              setTab={setActiveTab}
-              customize={state.customize}
-              showPanel={showPanel}
-            >
-              <div label='Static Icons'>
-                {state.iconsCategory.map((categoryObject, index) => {
-                  return categoryObject.icons.length > 0 ? (
-                    <div key={index}>
-                      <h4 className='category'>{categoryObject.category}</h4>
-                      <div className='icons-list'>
-                        {categoryObject.icons.map((icon, i) => (
-                          <Icon
-                            size={36}
-                            active={isActive(icon.name, state)}
-                            key={i}
-                            name={icon.name}
-                            action={() =>
-                              selectIcon(
-                                icon,
-                                dispatch({
-                                  type: state.customize
-                                    ? 'ADD_MULTIPLE_ICONS'
-                                    : '',
-                                  selection: icon.name
-                                })
-                              )
-                            }
-                          />
-                        ))}
-                      </div>
-                    </div>
-                  ) : (
-                    ''
-                  )
-                })}
-              </div>
-              <div label='Animated Icons'>
-                <div className='icons-list'>
-                  {state.animatedIcons.map((icon, index) => (
-                    <div className='icon-container' key={index}>
-                      <img
-                        src={require(`eos-icons/animated-svg/${icon}.svg`)}
-                        alt={icon}
-                        className={icon === iconSelected?.name ? 'active' : ''}
-                        onClick={() => {
-                          setIconSelected({ name: icon })
-                          setShowPanel(true)
-                        }}
-                      />
-                      {icon}
-                    </div>
-                  ))}
-                </div>
-              </div>
-            </Tabs>
+                ''
+              )
+            })}
           </div>
-        </>
-      )}
-    </AppContext.Consumer>
+          <div label='Animated Icons'>
+            <div className='icons-list'>
+              {state.animatedIcons.map((icon, index) => (
+                <div className='icon-container' key={index}>
+                  <img
+                    src={require(`eos-icons/animated-svg/${icon}.svg`)}
+                    alt={icon}
+                    className={icon === iconSelected?.name ? 'active' : ''}
+                    onClick={() => {
+                      setIconSelected({ name: icon })
+                      setShowPanel(true)
+                    }}
+                  />
+                  {icon}
+                </div>
+              ))}
+            </div>
+          </div>
+        </Tabs>
+      </div>
+    </>
   )
 }
 
