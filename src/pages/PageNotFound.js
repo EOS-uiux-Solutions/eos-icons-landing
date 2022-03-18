ed28212d749b69d6428e12f5ae56ba824c58d267
AnshumanDhiman <67755381+AnshumanDhiman@users.noreply.github.com>
2022-03-18 21:42:16 +0530

    Refactored the 404 page and added a button to link back to the Homepage

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Link } from '@reach/router'
+import Button from '../components/Button'
+
 const PageNotFound = () => {
   return (
-    <div>
-      <h1 className='heading-not-found'>404</h1>
-      <h1 className='not-found'>Oops! Something went wrong. Return home.</h1>
+    <div className='heading-not-found'>
+      <h1>404</h1>
+      <br />
+      <h1>Oops! Something went wrong.</h1>
+      <br />
+      <Link to='/'>
+        <Button>Return To Home</Button>
+      </Link>
     </div>
   )
 }
